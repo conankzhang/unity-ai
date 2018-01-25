9676682b004dc7ff1efa3919c9c98b3a65928838
conankzhang <u0409453@utah.edu>
2018-01-25 12:57:06 -0700

    Added boid scripts

---
+++ Assets/_Scripts/Boids/Steering/DynamicSteering/DynoSeek.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynoSeek : MonoBehaviour {
+
+    private SteeringParams sp;
+    private Goal goalObject;
+    private Transform goal;
+    private DynoSteering steering;
+
+    // Use this for initialization
+    void Start () {
+        sp = GetComponent<SteeringParams>();
+        goalObject = GetComponent<Goal>();
+    }
+	
+	// Update is called once per frame
+	public DynoSteering getSteering() {
+        steering = new DynoSteering();
+
+        goal = goalObject.getGoal();
+        steering.force = goal.position - transform.position;
+        steering.force.Normalize();
+        steering.force = steering.force * sp.MAXACCEL;
+        steering.torque = 0f;
+
+        return steering;
+	}
+}
